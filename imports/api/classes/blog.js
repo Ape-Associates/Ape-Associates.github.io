5e4b4bd787f7b090bef8b60a52491b514fc4b07f
Ben Partridge <ben.partridge249@gmail.com>
2018-04-06 11:00:53 +1200

    Promisify saving of blogs so it appears synchronous

--- imports/api/classes/blog.js
+++ imports/api/classes/blog.js
@@ -5,7 +5,6 @@ import { Meteor } from 'meteor/meteor';
 import { Blogs } from '../collections/blogs.js';
 
 export default Blog = Class.create({
-    //TODO: Figure out a way to wrap saves in promises without code duplication
     name: 'Blog',
     collection: Blogs,
     fields: {
@@ -24,59 +23,42 @@ export default Blog = Class.create({
         createdAt: Date
     },
     meteorMethods: {
+        safe_save() {
+            const self = this;
+            const promise =  new Promise(function(resolve, reject) {
+                self.save((err, id) => {
+                    if (err) {
+                        console.error("Failed to save blog to db");
+                        console.error(err);
+                        reject(err);
+                    } else {
+                        self.reload()
+                        resolve(self);
+                    }
+                });
+            });
+            return promise;
+        },
+
         create() {
             this.validate({ fields: ['title', 'body'] }, err => {
                 if (ValidationError.is(err)) {
-                    console.log("Blog object is not valid");
-                    console.log(err.error);
+                    console.error("Blog object is not valid");
+                    console.error(err.error);
                 }
             });
             //TODO: use meteor User framework
             this.author = 'Dummy Author';
             this.createdAt = new Date();
-            return this.save((err, id) => {
-                if (err && err.error) {
-                    console.log("Blog object could not save");
-                    console.log(err.error);
-                }
-            });
+            return this.safe_save();
         },
         rename(title) {
             this.title = title;
-            this.validate({ fields: ['title']}, err => {
-                if (ValidationError.is(err)) {
-                    console.log("New title is not valid: " + title);
-                    console.log(err.error);
-                }
-            });
-            // Returns a number (don't know what it means)
-            return this.save((err, id) => {
-                if (err && err.error) {
-                    console.log("Blog object could not save");
-                    console.log(err.error);
-                }
-            });
+            return this.safe_save();
         },
         modify(newContent) {
             this.body = newContent;
-            this.validate({ fields: ['body']}, err => {
-                if (ValidationError.is(err)) {
-                    console.log("New title is not valid: " + title);
-                    console.log(err.error);
-                }
-            });
-            // Returns the object but it's not thennable
-            return new Promise((resolve, reject) => {
-                this.save((err, id) => {
-                    if (ValidationError.is(err)) {
-                        console.log("New title is not valid: " + title);
-                        console.log(err.error);
-                        reject(err);
-                    } else {
-                        resolve(this);
-                    }
-                });
-            });
+            return this.safe_save();
         },
         delete() {
             return this.remove();

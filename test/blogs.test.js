c88901edd8e5b5db1472a5ed7c8c17e6625267d6
Ben Partridge <ben.partridge249@gmail.com>
2018-04-08 12:27:28 +1200

    Add tests for blog validators

--- test/blogs.test.js
+++ test/blogs.test.js
@@ -1,6 +1,7 @@
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { Mongo } from 'meteor/mongo';
 import { chai } from 'meteor/practicalmeteor:chai';
+import { ValidationError } from 'meteor/jagi:astronomy';
 
 import Blog from '../imports/api/classes/blog.js';
 
@@ -17,16 +18,203 @@ describe('Blogs', function() {
         blog.create();
     });
 
+    describe('validators', function() {
+        it('allows valid blogs', function() {
+            const valid_blog = new Blog({
+                title: 'Valid Title',
+                body: 'Valid Body'
+            });
+            const save_func = function() {
+                valid_blog.create();
+            }
+            chai.assert.doesNotThrow(save_func, /body|author|title|createdAt|/)
+        });
+
+        describe('title', function() {
+            it('must be a string', function() {
+                const bad_title_blog = new Blog({
+                    title: true
+                });
+                bad_title_blog.validate({
+                    fields: ['title'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isTrue(ValidationError.is(err));
+                    // TODO: Find way to put error messages in one place
+                    chai.assert.equal(err.reason, '"title" has to be a string');
+                });
+            });
+            it('can\'t be more than 60 characters', function() {
+                const bad_title_blog = new Blog({
+                    title: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+                });
+                bad_title_blog.validate({
+                    fields: ['title'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isTrue(ValidationError.is(err));
+                    // TODO: Find way to put error messages in one place
+                    chai.assert.equal(err.reason, 'Blog title can be at most 60 characters long');
+                });
+            });
+
+            it('can\'t be less than 3 characters', function() {
+                const bad_title_blog = new Blog({
+                    title: 'aa',
+                });
+                bad_title_blog.validate({
+                    fields: ['title'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isTrue(ValidationError.is(err));
+                    chai.assert.equal(err.reason, 'Blog title must be at least 3 characters long');
+                });
+            });
+
+            it('Can be between 3 and 60 characters', function() {
+                const minimum_title_blog = new Blog({
+                    title: 'aaa'
+                });
+                minimum_title_blog.validate({
+                    fields: ['title'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isUndefined(err);
+                });
+
+                const max_title_blog = new Blog({
+                    title: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+                });
+                max_title_blog.validate({
+                    fields: ['title'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isUndefined(err);
+                })
+
+                const title_blog = new Blog({
+                    title: 'A typical title for a typical blog post.'
+                });
+                title_blog.validate({
+                    fields: ['title'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isUndefined(err);
+                });
+            })
+        });
+
+        describe('body', function() {
+            it('must be a string', function() {
+                const bad_body_blog = new Blog({
+                    body: true
+                });
+                bad_body_blog.validate({
+                    fields: ['body'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isTrue(ValidationError.is(err));
+                    // TODO: Find way to put error messages in one place
+                    chai.assert.equal(err.reason, '"body" has to be a string');
+                });
+            });
+            it('can\'t be more than 16384 characters', function() {
+                let bad_body = "";
+                for (let i = 0; i < 16385; i++) {
+                    bad_body += "a";
+                }
+
+                const bad_body_blog = new Blog({
+                    body: bad_body
+                });
+
+                bad_body_blog.validate({
+                    fields: ['body'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isTrue(ValidationError.is(err));
+                    // TODO: Find way to put error messages in one place
+                    chai.assert.equal(err.reason, 'Blog post can be at most 16384 characters long');
+                });
+            });
+
+            it('can be less than or equal to 16384 characters', function() {
+                let long_body = "";
+                for (let i = 0; i < 16384; i++) {
+                    long_body += "a";
+                }
+                const long_blog = new Blog({
+                    body: long_body
+                });
+
+                let med_body = "";
+                for (let i = 0; i < 3000; i++) {
+                    long_body += "a";
+                }
+                const med_blog = new Blog({
+                    body: med_body
+                });
+
+                let short_body = "a";
+                const short_blog = new Blog({
+                    body: short_body
+                });
+                long_blog.validate({
+                    fields: ['body'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isUndefined(err);
+                });
+
+                med_blog.validate({
+                    fields: ['body'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isUndefined(err);
+                });
+
+                short_blog.validate({
+                    fields: ['body'],
+                    simulation: false,
+                },
+                err => {
+                    chai.assert.isUndefined(err);
+                });
+            });
+        });
+    });
+
     describe('#create', function() {
         it('Is created in the \'Blogs\' collection', function() {
             const queryBlog = Blog.findOne({ title: testTitle });
             chai.assert.equal(queryBlog.get('title'), testTitle);
             chai.assert.equal(queryBlog.get('body'), testBody);
         });
+
+        it('Is created with a current timestamp', function() {
+            let blog = new Blog({
+                title: testTitle,
+                body: testBody
+            });
+            const created_time = new Date();
+            blog = blog.create();
+            const time_diff = Math.abs(created_time.getTime() - blog.get('createdAt').getTime());
+            chai.assert.isBelow(time_diff, 1000);
+        })
     });
 
     describe('#rename', function() {
-        it('changes the title of the blog post', function() {
+        it('Changes the title of the blog post', function() {
             let blog = Blog.findOne({ title: testTitle });
             const id = blog.get('_id');
 
@@ -39,7 +227,7 @@ describe('Blogs', function() {
     });
 
     describe('#modify', function() {
-        it('changes the content of the blog post', function() {
+        it('Changes the content of the blog post', function() {
             let blog = Blog.findOne({ title: testTitle });
             const id = blog.get('_id');
 
@@ -52,7 +240,7 @@ describe('Blogs', function() {
     });
 
     describe('#delete', function() {
-        it('removes the blog from the database', function() {
+        it('Removes the blog from the database', function() {
             let blog = Blog.findOne({ title: testTitle })
             const id = blog.get('_id');
 

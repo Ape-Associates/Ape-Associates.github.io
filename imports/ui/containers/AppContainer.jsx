7d498ffeaf97edef1914ee3dee58464b75005732
Ben Partridge <ben.partridge249@gmail.com>
2018-04-10 12:45:39 +1200

    Remove code to inject blogs from appcontainer. It's not ready and not parpart of testing

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -5,14 +5,12 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import App from '../../ui/layouts/App.jsx';
-//import { Blogs } from '../../api/collections/blogs.js';
 
 export default withTracker(() => {
     return {
         // TODO: implement users
         // user: Meteor.user(),
         connected: Meteor.status().connected,
-        //blogs: Blogs.find({}).fetch(),
         // TODO: add blog posts to tracking
     };
 })(App);

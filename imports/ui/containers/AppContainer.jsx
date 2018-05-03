d84a3e91a4ceb0aa603a422e20c0f7c17ae7cce1
Ben Partridge <ben.partridge249@gmail.com>
2018-05-03 18:46:01 +1200

    Implement subscription in App Container

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -5,12 +5,21 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import App from '../../ui/layouts/App.jsx';
+import Blog from '../../api/blogs/blog.js';
 
 export default withTracker(() => {
+    const visibleHandle = Meteor.subscribe('blogs.visible');
+    const myDraftsHandle = Meteor.subscribe('blogs.myDrafts');
+
     return {
         // TODO: implement users
         // user: Meteor.user(),
+        loading: !visibleHandle.ready() || !myDraftsHandle.ready(),
         connected: Meteor.status().connected,
-        // TODO: add blog posts to tracking
+        blogs: Blog.find({ $or: [
+            { isDraft: false },
+            { isDraft: true, userId: Meteor.userId() }
+        ]}, { sort: { createdAt: -1 } }).fetch(),
+        latest: Blog.findOne({ isDraft: false }, { sort: { createdAt: -1} })
     };
 })(App);

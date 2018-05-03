59a46d041584f9fa6f472181219e0e1bba3ceb93
Ben Partridge <ben.partridge249@gmail.com>
2018-05-03 18:01:21 +1200

    Implement visible blogs publication

--- imports/api/blogs/server/publications.js
+++ imports/api/blogs/server/publications.js
@@ -2,6 +2,10 @@ import { Meteor } from 'meteor/meteor';
 
 import Blog from '../blog.js';
 
+Meteor.publish('blogs.visible', function blogsVisible() {
+    return Blog.find({ isDraft: false }, { sort: { createdAt: -1 } });
+});
+
 Meteor.publish('blogs.latest', function blogsLatest() {
     return Blog.findOne({ isDraft: false }, { sort: { createdAt: -1 } });
 });

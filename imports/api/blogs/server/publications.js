48f31d92edbc6f0dbb1911bf04899dd17bc00036
Ben Partridge <ben.partridge249@gmail.com>
2018-05-03 18:46:25 +1200

    Remove unnecessary subscriptions

--- imports/api/blogs/server/publications.js
+++ imports/api/blogs/server/publications.js
@@ -3,15 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import Blog from '../blog.js';
 
 Meteor.publish('blogs.visible', function blogsVisible() {
-    return Blog.find({ isDraft: false }, { sort: { createdAt: -1 } });
-});
-
-Meteor.publish('blogs.latest', function blogsLatest() {
-    return Blog.findOne({ isDraft: false }, { sort: { createdAt: -1 } });
-});
-
-Meteor.publish('blogs.byAuthor', function blogsByAuthor(authorId) {
-    return Blog.find({ author: authorId, isDraft: false }, { sort: { createdAt: -1 } });
+    return Blog.find({ isDraft: false });
 });
 
 Meteor.publish('blogs.myDrafts', function blogsMyDrafts() {
@@ -19,5 +11,5 @@ Meteor.publish('blogs.myDrafts', function blogsMyDrafts() {
         return this.ready();
     }
 
-    return Blog.find({ author: this.userId, isDraft: true }, { sort: { createdAt: -1 } });
+    return Blog.find({ author: this.userId, isDraft: true });
 });

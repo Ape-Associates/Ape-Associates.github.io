fffd33012635b7666d5aafe6cb7a41384b3ae84b
Ben Partridge <ben.partridge249@gmail.com>
2018-05-03 17:55:25 +1200

    Add by author blog publication

--- imports/api/blogs/server/publications.js
+++ imports/api/blogs/server/publications.js
@@ -3,12 +3,9 @@ import { Meteor } from 'meteor/meteor';
 import Blog from '../blog.js';
 
 Meteor.publish('blogs.latest', function blogsLatest() {
-    return Blog.findOne({
-        isDraft: false
-    },
-    {
-        sort: {
-            createdAt: -1
-        }
-    });
+    return Blog.findOne({ isDraft: false }, { sort: { createdAt: -1 } });
 });
+
+Meteor.publish('blogs.byAuthor', function blogsByAuthor(authorId) {
+    return Blog.find({ author: authorId }, { sort: { createdAt: -1 } });
+}
